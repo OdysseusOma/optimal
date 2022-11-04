a4800d6c8a371b2a79ca476f538e1a5abf6e90d2
Adebayo Omirin <adebayoomirin@gmail.com>
2022-11-05 00:01:40 +0100

    second update with click bug

--- components/ThreeD.jsx
+++ components/ThreeD.jsx
@@ -15,7 +15,7 @@ const ThreeD = () => {
       </div>
       <div className='flex flex-col items-center justify-evenly w-full h-[30%]'>
         <h1 className='font-bold text-2xl max-w-[500px] text-center'>
-          There is so much to see, don't worry, we'll get you there
+          There is so much to see, don\'t worry, we\'ll get you there
         </h1>
       </div>
     </div>
