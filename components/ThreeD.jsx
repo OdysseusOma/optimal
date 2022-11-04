b20ee186fe5d96d7b38d7b22b760b9969a3b638d
Adebayo Omirin <adebayoomirin@gmail.com>
2022-11-05 00:21:20 +0100

    second update with click bug

--- components/ThreeD.jsx
+++ components/ThreeD.jsx
@@ -4,6 +4,7 @@ import { Suspense } from 'react'
 import Earth from './Earth'
 
 const ThreeD = () => {
+   /* eslint-disable no-eval */
   return (
     <div className='h-[80vh] bg-[#00000e]'>
       <div className='h-[70%]'>
@@ -14,7 +15,9 @@ const ThreeD = () => {
         </Canvas>
       </div>
       <div className='flex flex-col items-center justify-evenly w-full h-[30%]'>
-        
+        <h1 className='font-bold text-2xl max-w-[500px] text-center'>
+          There is so much to see, don't worry, we'll get you there
+        </h1>
       </div>
     </div>
   )
